2801401f0115e4f1ed6ef0a521eab9f4b403dbf6
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2023-02-11 22:38:06 +0300

    Challenge_01 solved

---
+++ Chapter_01/Challenge_01.py
@@ -0,0 +1,8 @@
+# Chapter 1, Challenge 2
+# Write and save a program that prints out your name and waits for the user to press the 
+# Enter key before the program ends. Then, run the program by double-clicking its icon.
+print('Game over')
+input('\n\nНажмите Enter. чтобы выйти. ')
+
+print('Georgy')
+input('Press enter to exit program.')
