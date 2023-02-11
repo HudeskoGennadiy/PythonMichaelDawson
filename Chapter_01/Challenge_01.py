4d9e17ed547aaf0c6c9e9e2735633041765d16ef
Gennadiy Khudesko <gennadiy.18224@gmail.com>
2023-02-11 23:15:17 +0300

    Challenge_02 solved

--- Chapter_01/Challenge_01.py
+++ Chapter_01/Challenge_01.py
@@ -1,8 +1,9 @@
-# Chapter 1, Challenge 2
-# Write and save a program that prints out your name and waits for the user to press the 
-# Enter key before the program ends. Then, run the program by double-clicking its icon.
-print('Game over')
-input('\n\nНажмите Enter. чтобы выйти. ')
-
-print('Georgy')
-input('Press enter to exit program.')
+# Задача №1
+# Научитесь вызывать ошибку: в
+# интерактивном режиме введите 
+# название своего любимого сорта мороженого. 
+# Потом исправьте ошибку и создайте команду, 
+# которая будет правильным образом выводить
+# на экран название этого сорта мороженого.
+# Эскимо
+print('Эскимо')
